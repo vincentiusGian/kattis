c79ad2cf871771703f69ce7dad86da804a6736ac
vincentiusgian <gianjunius54@gmail.com>
2026-07-06 18:17:41 +0700

    sequences added

---
+++ sequences.py
@@ -0,0 +1,26 @@
+# Written by Vincent
+
+# Observation -> ini soal math banget!!
+# Bakal dikasi k "?" sehingga ada len(s)-k angka pasti (0 atau 1)
+# ada 2^k kemungkinan sequences!???  
+
+
+
+MOD = int(1e9+7)
+
+s = input()
+one = 0 
+branch = 1
+inv = 0 
+
+for c in s:
+    if c == "1":
+        one = (one+branch) % MOD
+    elif c == "0":
+        inv = (inv+one)%MOD
+    else:
+        inv = (2*inv + one)%MOD
+        one = (2*one + branch)%MOD 
+        branch = 2*branch % MOD
+
+print(inv)
