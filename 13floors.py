8b99a1b9038572867d4a7a943b845c6fa724fbd3
vincentiusgian <gianjunius54@gmail.com>
2026-07-07 15:02:14 +0700

    adding 13floors.py

---
+++ 13floors.py
@@ -0,0 +1,6 @@
+x = int(input())
+
+if x < 13:
+    print(x)
+else:
+    print(x+1)
--- sequences.py
+++ sequences.py
@@ -10,7 +10,7 @@ inv = 0
 for c in s:
     if c == "1":
         one = (one+branch) % MOD
-    elif c == "0":
+    elif c == "0":   
         inv = (inv+one)%MOD
     else:
         inv = (2*inv + one)%MOD
